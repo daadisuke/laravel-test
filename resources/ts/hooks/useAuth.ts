2e3bc04a4adbb5d784b048d70ec973e78f97b449
daisuke-ito <daisuke_ito@012grp.co.jp>
2023-12-29 15:39:31 +0900

    axiosファイル作成

---
+++ resources/ts/hooks/useAuth.ts
@@ -0,0 +1,14 @@
+import * as api from '@/api/authAPI'
+import { queryClient } from '@/queryClient'
+ 
+const authUserQuery = () => ({
+    queryKey: ['user'],
+    queryFn: api.getUser
+})
+ 
+export const useAuthUser = async () => {
+    const query = authUserQuery()
+ 
+    return queryClient.getQueryData(query.queryKey)
+        ?? (await queryClient.fetchQuery(query).catch(() => undefined))
+}
